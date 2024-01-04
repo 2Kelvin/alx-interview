6771f592a3c7195ed703260fbc7e49e804b40d96
2Kelvin <rocketmancodes02@gmail.com>
2024-01-04 21:55:26 +0300

    pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+'''Pascal's triangle'''
+
+
+def pascal_triangle(n):
+    '''Returns a list of lists of integers representing Pascals triangle of n'''
+    if n <= 0:
+        return []
+
+    tri = [[1]]
+
+    for _ in range(1, n):
+        pre = tri[-1]
+        cur = [pre[0]]
+
+        for i in range(1, len(pre)):
+            cur.append(pre[i - 1] + pre[i])
+
+        cur.append(pre[-1])
+        tri.append(cur)
+
+    return tri
