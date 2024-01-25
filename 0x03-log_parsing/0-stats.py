830e103a658d2267f198adc9c959ec6e8a7b7d26
2Kelvin <rocketmancodes02@gmail.com>
2024-01-25 15:39:10 +0300

    you bore me

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,11 +2,19 @@
 '''Log parsing'''
 import sys
 
-dictCodes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
-                     '404': 0, '405': 0, '500': 0}
+dictCodes = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
 
 tSize = 0
-count = 0 
+count = 0
 
 try:
     for line in sys.stdin:
@@ -22,15 +30,15 @@ try:
         if count == 10:
             count = 0
             print('File size: {}'.format(tSize))
-            for key, value in sorted(dictCodes.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
+            for x, y in sorted(dictCodes.items()):
+                if y != 0:
+                    print('{}: {}'.format(x, y))
 
 except Exception as err:
     pass
 
 finally:
     print('File size: {}'.format(tSize))
-    for key, value in sorted(dictCodes.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+    for x, y in sorted(dictCodes.items()):
+        if y != 0:
+            print('{}: {}'.format(x, y))
