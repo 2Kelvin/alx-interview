e21016cce04082aabe6c3e45adafaa052db914ac
2Kelvin <rocketmancodes02@gmail.com>
2024-02-13 01:14:44 +0300

    display the film characters in order :muscle:

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,20 +4,29 @@ const request = require('request');
 const movieID = process.argv[2];
 const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 
-// use the url to fetch the film
-request(filmUrl, (err, response, body) => {
-    if (err) console.error(err);
-    // console.log(body);
-    data = JSON.parse(body);
+function recursiveXterNameFetch(allXtersUrl) {
+    if (allXtersUrl.length === 0) return;
+
+    // chop off, return and use each character url from the start one by one
+    const xterUrl = allXtersUrl.shift();
+    request(xterUrl, (err, response, body) => {
+        if (err) console.error(err);
+        const xterObject = JSON.parse(body);
+        console.log(xterObject.name);
+
+        // after displaying first character name, move to the next character
+        // remember shift removes the first element, returns it and ...
+        // ... shifts the array items i.e the 2nd character is now the 1st
+        // ...the 3rd is the 2nd and so on
+        recursiveXterNameFetch(allXtersUrl);
+    });
+}
+
+request(filmUrl, (error, resp, bdy) => {
+    if (error) console.error(error);
+    data = JSON.parse(bdy);
     const movieXtersArray = data.characters;
-    // console.log(movieXtersArray);
-    for (const xterUrl of movieXtersArray) {
-        request(xterUrl, (er, resp, bdy) => {
-            if (er) console.error(er);
-            const xterData = JSON.parse(bdy);
-            console.log(xterData.name);
-        });
-    }
+
+    // make a copy of the api film characters as it is
+    recursiveXterNameFetch(movieXtersArray.slice());
 });
-// access all characters through => .characters which is a list
-// loop thro array, printing characters
