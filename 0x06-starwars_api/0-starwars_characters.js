91c6375bbc81c15a783e1126b06182cefc370f66
2Kelvin <rocketmancodes02@gmail.com>
2024-02-13 01:25:23 +0300

    semistandard approved :thumbsup:

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,29 +4,29 @@ const request = require('request');
 const movieID = process.argv[2];
 const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 
-function recursiveXterNameFetch(allXtersUrl) {
-    if (allXtersUrl.length === 0) return;
+function recursiveXterNameFetch (allXtersUrl) {
+  if (allXtersUrl.length === 0) return;
 
-    // chop off, return and use each character url from the start one by one
-    const xterUrl = allXtersUrl.shift();
-    request(xterUrl, (err, response, body) => {
-        if (err) console.error(err);
-        const xterObject = JSON.parse(body);
-        console.log(xterObject.name);
+  // chop off, return and use each character url from the start one by one
+  const xterUrl = allXtersUrl.shift();
+  request(xterUrl, (err, response, body) => {
+    if (err) console.error(err);
+    const xterObject = JSON.parse(body);
+    console.log(xterObject.name);
 
-        // after displaying first character name, move to the next character
-        // remember shift removes the first element, returns it and ...
-        // ... shifts the array items i.e the 2nd character is now the 1st
-        // ...the 3rd is the 2nd and so on
-        recursiveXterNameFetch(allXtersUrl);
-    });
+    // after displaying first character name, move to the next character
+    // remember shift removes the first element, returns it and ...
+    // ... shifts the array items i.e the 2nd character is now the 1st
+    // ...the 3rd is the 2nd and so on
+    recursiveXterNameFetch(allXtersUrl);
+  });
 }
 
 request(filmUrl, (error, resp, bdy) => {
-    if (error) console.error(error);
-    data = JSON.parse(bdy);
-    const movieXtersArray = data.characters;
+  if (error) console.error(error);
+  const data = JSON.parse(bdy);
+  const movieXtersArray = data.characters;
 
-    // make a copy of the api film characters as it is
-    recursiveXterNameFetch(movieXtersArray.slice());
+  // make a copy of the api film characters as it is
+  recursiveXterNameFetch(movieXtersArray.slice());
 });
