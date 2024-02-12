f8b8f3ebc670ff34a37baeed5ff8b98593abd300
2Kelvin <rocketmancodes02@gmail.com>
2024-02-13 00:12:18 +0300

    fetch films

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
+const request = require('request');
 
-// import request here
+const movieID = process.argv[2];
+const filmUrl = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
 
-const filmUrl = 'https://swapi-api.alx-tools.com/api/films/';
+// use the url to fetch the film
+request(filmUrl, (err, body) => {
+    if (err) console.error(err);
+    console.log(data);
+});
+// access all characters through => .characters which is a list
+// loop thro array, printing characters
